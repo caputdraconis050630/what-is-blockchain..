b34e71ee690ce63c191a3531bb837efe3b86c16e
caputdraconis <caputdraconis@kakao.com>
2025-01-25 21:47:42 +0900

    create dummy blockchain

--- main.go
+++ main.go
@@ -1,6 +1,17 @@
 package main
 
+import "fmt"
+
 func main() {
-	cli := CLI{}
-	cli.Run()
+	bc := NewBlockchain() // 블록체인을 생성한다.
+
+	bc.AddBlock("Send 1 BTC to caput")
+	bc.AddBlock("Send 2 more BTC to caput")
+
+	for _, block := range bc.blocks {
+		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		fmt.Println()
+	}
 }
